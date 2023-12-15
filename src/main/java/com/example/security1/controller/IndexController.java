c84ff619364e25c9e1e5e4251e3e608b2f7d329f
CSeungJoo <seungjoo13579@gmail.com>
2023-12-15 21:16:06 +0900

    스프링부트 시큐리티 1강 - 환경설정

---
+++ src/main/java/com/example/security1/controller/IndexController.java
@@ -0,0 +1,16 @@
+package com.example.security1.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Controller
+public class IndexController {
+    @GetMapping({"", "/"})
+    public String index() {
+
+        // 머스테치 기본폴더 src/main/resources
+        // 뷰리졸버 설정: templates (prefix), .mustache (suffix) 생략가능!!
+        return "index";
+    }
+}
